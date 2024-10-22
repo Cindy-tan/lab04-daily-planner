04c5d40bfdfb55298f9af99a2ee694054992f3d0
Cindy-tan <ctanhe@my.bcit.ca>
2024-10-21 18:14:04 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Task from './components/Task';
+import TaskForm from './components/TaskForm';
+
+function DayPlannerApp() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskName) => {
+    setTasks([...tasks, { name: taskName, completed: false }]);
+  };
+
+  const toggleTaskCompletion = (index) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const remainingTasksCount = tasks.filter((task) => !task.completed).length;
+
+  return (
+    <div className="formContainer">
+      <h1>Daily Planner</h1>
+      
+      <TaskForm addTask={addTask} />
+      <h2>You have {remainingTasksCount} tasks remaining </h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <Task
+            key={index}
+            task={task}
+            index={index}
+            toggleTaskCompletion={toggleTaskCompletion}
+            deleteTask={deleteTask}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DayPlannerApp;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styles from './TaskForm.module.css'
+
+function TaskForm({ addTask }) {
+  const [taskName, setTaskName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskName.trim()) {
+      addTask(taskName);
+      setTaskName('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a new task"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+        className={styles.placeholder}
+      />
+      &nbsp;
+      &nbsp;
+      <button type="submit" className={styles.saveBtn}>Save</button>
+    </form>
+  );
+}
+
+export default TaskForm;
